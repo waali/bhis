305e0463c3a9d8c441d1a2c2bf4f59595897a8d4
Seto El Kahfi <setoelkahfi@gmail.com>
2014-08-28 13:36:20 +0700

    Added slider home front-end

--- controller/home.php
+++ controller/home.php
@@ -1,8 +1,40 @@
+<style>
+.theme-default #slider {
+   margin-right: auto;
+   margin-left: auto;
+   width:618px; /* Make sure your images are the same size */
+   height:246px; /* Make sure your images are the same size */
+}
+</style>
 <?php
-$data 	= mysql_query("select * from home");
-$no 	= 1;
-$tampil = mysql_fetch_array($data);
-echo '<h1>'.$tampil['judul'].'</h1>';
-echo $tampil['isi'];
+$Q_home	= mysql_query("SELECT * FROM home");
+$home 	= mysql_fetch_array($Q_home);
+
+$Q_slide= mysql_query("SELECT * FROM slide");
+;
 ?>
+<h1><?php echo $home['judul']; ?></h1>
+<?php if (mysql_num_rows($Q_slide) > 0) : ?>
+	<div class="slider-wrapper theme-default">
+		<div id="slider" class="nivoSlider">
+			<?php while ($slide = mysql_fetch_array($Q_slide)) : ?>
+				<?php if ($slide['link'] == '') : ?>
+					<img src="images/slide/<?php echo $slide['gambar']; ?>" alt="" width="100%" alt="<?php echo $slide['judul']; ?>" title="<?php echo $slide['keterangan']; ?>"/>
+				<?php else : ?>
+					<a href="<?php echo $slide['link']; ?>"><img src="images/slide/<?php echo $slide['gambar']; ?>" alt="<?php echo $slide['judul']; ?>" title="<?php echo $slide['keterangan']; ?>" /></a>
+				<?php endif; ?>
+			<?php endwhile; ?>
+		</div>
+	</div>
+<?php endif; ?>
+<div>
+<?php
+	echo $home['isi'];
+?>
+</div>
 <div class="more_button"><a href="index.php?pg=pendaftaran">Pendaftaran</a></div>
+<script type="text/javascript">
+$(window).load(function() {
+    $('#slider').nivoSlider();
+});
+</script>
--- index.php
+++ index.php
@@ -11,9 +11,14 @@ include 'inc/redirection.php';
 		<meta name="keywords" content="" />
 		<meta name="description" content="" />
 		<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/ui-lightness/jquery-ui-1.9.2.custom.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/ui-lightness/jquery-ui-1.9.2.custom.min.css" />		
+		<link rel="stylesheet" href="assets/css/themes/default/default.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="assets/css/themes/orman/orman.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="assets/css/themes/pascal/pascal.css" type="text/css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="assets/css/nivo-slider.css" />
 		<script type="text/javascript" src="assets/js/jquery-1.8.3.js"></script>
 		<script type="text/javascript" src="assets/js/jquery-ui-1.9.2.custom.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery.nivo.slider.pack.js"></script>
 		<style type="text/css">
 		#templatemo_login {
 			float: right;
